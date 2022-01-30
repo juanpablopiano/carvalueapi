d1ddefdaa788210493d5fd7bb75972ffc5fe68d6
Juan Gonzalez <jgu1@hotmail.com>
2022-01-30 19:52:38 -0300

    Finish implementing remove service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -28,12 +28,21 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      // Throw not found exception
+      throw new NotFoundException(`User with id ${id} not found`);
+      // throw new Error('User not found');
     }
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
-
+  
   // Delte user with given id
-  remove() {}
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+      // throw new Error('User not found');
+    }
+    return this.repo.remove(user);
+  }
 }
