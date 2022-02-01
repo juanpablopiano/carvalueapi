1b621653bc9a584cf7414b420ec2f9e8eda8dfc5
Juan Gonzalez <jgu1@hotmail.com>
2022-01-31 23:34:37 -0300

    Make interceptor more reusable

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,14 +6,16 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
-import { UserDto } from 'src/users/dtos/user.dto';
+import { plainToInstance } from 'class-transformer';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
-        return plainToClass(UserDto, data, {
+        // return plainToClass(this.dto, data, { this is deprecated
+        return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
       })
