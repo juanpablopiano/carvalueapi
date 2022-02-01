b5bee95bacc2cf05dba71e672e99cc4b4eca0b93
Juan Gonzalez <jgu1@hotmail.com>
2022-01-31 22:29:13 -0300

    Implement nest password serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,8 @@ import {
   Query,
   Param,
   NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -21,6 +23,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
