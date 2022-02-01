8b6ec68cf74f159f77bb11c71b94a1844d669c9f
Juan Gonzalez <jgu1@hotmail.com>
2022-01-31 23:29:14 -0300

    Implement custom serialization for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,11 +9,11 @@ import {
   Param,
   NotFoundException,
   UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -23,7 +23,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
