83b1bf79853b7bc378d588aa500fbee9e19ec53f
Juan Gonzalez <jgu1@hotmail.com>
2022-02-03 23:32:31 -0300

    Add a duplicated email test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,10 +5,11 @@ import { User } from './user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -29,5 +30,25 @@ describe('AuthService', () => {
 
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('creates a new user with salted and hashed password', async () => {
+    const password = 'password1';
+    const user = await service.signup('email@email.com', password);
+
+    expect(user.password).not.toEqual(password);
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async done => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    try {
+      await service.signup('email@email.com', 'password');
+    } catch (error) {
+      done();
+    }
   });
 });
