40bce4186c7f90b385a44b6a2b6433b93f7eeb45
Juan Gonzalez <jgu1@hotmail.com>
2022-02-07 23:51:54 -0300

    Configure e2e tests to delete the db before every test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -29,4 +29,19 @@ describe('Athentication System (e2e)', () => {
         expect(email).toBe(emailOrig);
       });
   });
+
+  it('handles a signup request', () => {
+    const emailOrig = 'test2@email.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: emailOrig, password: 'test1' })
+      .expect(201)
+      .then(res => {
+        const { id, email } = res.body;
+
+        expect(id).toBeDefined();
+
+        expect(email).toBe(emailOrig);
+      });
+  });
 });
