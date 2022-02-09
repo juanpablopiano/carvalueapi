eca6214c8b39f1fc9a2a5e113ab4a57bbab261e3
Juan Gonzalez <jgu1@hotmail.com>
2022-02-08 23:44:13 -0300

    Finish implementing create report service and create request http file to test api

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
 export class ReportsService {
-  create(body) {
-    return 'Report';
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+
+    return this.repo.save(report);
   }
 }
-
